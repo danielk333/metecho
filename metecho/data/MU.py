8b42aa05e95306d1857491d8941bf7cb959475a8
kandarian <kenneth@kullbrandt.se>
2021-09-23 11:27:07 +0200

    made functions private and made some integers unsigned where the input text described it as such.

--- metecho/data/MU.py
+++ metecho/data/MU.py
@@ -19,13 +19,13 @@ def _get_header_data(fid):
     %-- -> 8x8=64 bits and is written in ASCII[sic] (the format is decoded with
     %-- UTF-8, so I must assume it is encoded as such).
     """
-    header_data["program_name"] = decode_utf(
+    header_data["program_name"] = _decode_utf(
         np.fromfile(fid, dtype='S8', count=1, offset=24))
 
     """
     %-- (33-56) Parameter file load time: 24 bytes, of characters: dd-mmm-yyyy hh:mm:ss(.ss?)
     """
-    header_data["file_load_time"] = np.datetime64(convert_date(
+    header_data["file_load_time"] = np.datetime64(_convert_date(
         np.fromfile(fid, dtype='S24', count=1, offset=32)), 'ns')
 
     """
@@ -37,7 +37,7 @@ def _get_header_data(fid):
     """
     %-- (61-84) Record start time: DD-MMM-YYYY hh:mm:ss.ss is 24 bytes long, each 8 bits
     """
-    header_data["record_start_time"] = np.datetime64(convert_date(
+    header_data["record_start_time"] = np.datetime64(_convert_date(
         np.fromfile(fid, dtype='S24', count=1, offset=60)), 'ns')
 
     """
@@ -49,9 +49,9 @@ def _get_header_data(fid):
     %-- hasn't occured.
     """
     start_date = np.datetime_as_string(header_data["record_start_time"])[0:10]
-    end_time = decode_utf(np.fromfile(fid, dtype='S12', count=1, offset=84))
+    end_time = _decode_utf(np.fromfile(fid, dtype='S12', count=1, offset=84))
     temp_end_time = np.datetime64(start_date + " " + end_time, 'ns')
-    header_data["record_end_time"] = fix_date_edge_case(header_data["record_start_time"], temp_end_time)
+    header_data["record_end_time"] = _fix_date_edge_case(header_data["record_start_time"], temp_end_time)
     """
     %-- (97-172) The next 19 numbers are 4 byte integers, i.e. 4*8=32 bits long
     """
@@ -61,13 +61,13 @@ def _get_header_data(fid):
     %-- i.e. each longword is 84/21=4 bytes of 8 bits each, i.e., the same thing as matlab's int32
     """
 
-    header_data["lag_number_per_ACF"] = np.fromfile(fid, dtype=np.int32, count=21, offset=172)
+    header_data["lag_number_per_ACF"] = np.fromfile(fid, dtype=np.uint32, count=21, offset=172)
     """
     %-- (257-320) The beam direction number in first 16 beams is represented by 16 unsigned longwords
     %-- of 4 bytes and 4*8=32 bits each
     """
 
-    header_data["beam_direction_number"] = np.fromfile(fid, dtype=np.int32, count=16, offset=256)
+    header_data["beam_direction_number"] = np.fromfile(fid, dtype=np.uint32, count=16, offset=256)
     """
     %-- (321-336) The next 4 parameters are 4 byte, 4*8=32 bit, integers
     """
@@ -77,7 +77,7 @@ def _get_header_data(fid):
     %-- cannot add characters to the 'head' matrix
     """
 
-    header_data["hp_file_name"] = decode_utf(np.fromfile(fid, dtype='S8', count=1, offset=336))
+    header_data["hp_file_name"] = _decode_utf(np.fromfile(fid, dtype='S8', count=1, offset=336))
     """
     %-- (345-352) The next two parameters are 4 byte, i.e. 4*8=32 bit, integers
     """
@@ -87,7 +87,7 @@ def _get_header_data(fid):
     %-- (353-416) The number of sum (ACF method): 16 unsigned longwords -> 16 * 4 byte (4*8=32 bit) integers
     """
 
-    header_data["number_of_sum_ACF"] = np.fromfile(fid, dtype=np.int32, count=16, offset=352)
+    header_data["number_of_sum_ACF"] = np.fromfile(fid, dtype=np.uint32, count=16, offset=352)
     """
     %-- (417-420) Number of sample poinst: 4 bytes = 32 bits
     """
@@ -98,7 +98,7 @@ def _get_header_data(fid):
     %-- (601-616) Observation parameter name: 16 bytes
     """
 
-    header_data["observation_param_name"] = decode_utf(np.fromfile(fid, dtype='S16', count=1, offset=600))
+    header_data["observation_param_name"] = _decode_utf(np.fromfile(fid, dtype='S16', count=1, offset=600))
     """
     %-- (617-640) The next 6 parameters are 4 bytes (4*8=32bits) each
     """
@@ -139,7 +139,7 @@ def _get_header_data(fid):
     %-- (1697-1716) TX frequency offset of 5 unsigned longwords -> 5 * 4 byte (4*8=32 bit) integers
     """
 
-    header_data["tx_frequency"] = np.fromfile(fid, dtype=np.int32, count=5, offset=1696)
+    header_data["tx_frequency"] = np.fromfile(fid, dtype=np.uint32, count=5, offset=1696)
     """
     %-- (1717-1724) The next 2 parameters are 4 bytes (2*8=32 bits) each
     """
@@ -149,7 +149,7 @@ def _get_header_data(fid):
     %-- (1725-1740) The RX attenuator is 4 unsigned longwords -> 4 * 4 byte (4*8=32 bit) integers
     """
 
-    header_data["rx_attentuator"] = np.fromfile(fid, dtype=np.int32, count=4, offset=1724)
+    header_data["rx_attentuator"] = np.fromfile(fid, dtype=np.uint32, count=4, offset=1724)
     """
     %-- (1741-1744) The state of the TX is given by a 4 byte (4*8=32 bit) integer
     """
@@ -159,7 +159,7 @@ def _get_header_data(fid):
     %-- (1745-1760) The state of the RX is given by 4 unsigned longwords -> 4 * 4 byte (4*8=32 bit) integers
     """
 
-    header_data["rx_state"] = np.fromfile(fid, dtype=np.int32, count=4, offset=1744)
+    header_data["rx_state"] = np.fromfile(fid, dtype=np.uint32, count=4, offset=1744)
     """
     %-- (1761:1812) The RX module selection is given by 2 bytes * (25+1)
     %-- -> cannot be saved in the head matrix (no room for it)
@@ -176,7 +176,7 @@ def _get_header_data(fid):
     %-- unsigned longwords (unit of sub-pulse/4) -> 256 * 4 byte (4*8=32 bit) integer
     """
 
-    header_data["sample_start_time"] = np.fromfile(fid, dtype=np.int32, count=256, offset=1820)
+    header_data["sample_start_time"] = np.fromfile(fid, dtype=np.uint32, count=256, offset=1820)
     """
     %-- (2845-2848) The reception sequence: 4 byte (4*8=32 bit) integer
     """
@@ -192,19 +192,19 @@ def _get_header_data(fid):
     %-- 29 unsigned longwords -> 29 * 4 byte (4*8=32 bit) integers
     """
 
-    header_data["coherent_integrations"] = np.fromfile(fid, dtype=np.int32, count=29, offset=2964)
+    header_data["coherent_integrations"] = np.fromfile(fid, dtype=np.uint32, count=29, offset=2964)
     """
     %-- (3081-3196) Number of FFT points for each combined channel:
     %-- 29 unsigned longwords -> 29 * 4 byte (4*8=32 bit) integers
     """
 
-    header_data["fft_points"] = np.fromfile(fid, dtype=np.int32, count=29, offset=3080)
+    header_data["fft_points"] = np.fromfile(fid, dtype=np.uint32, count=29, offset=3080)
     """
     %-- (3197-3312) Number of data points for each combined channel:
     %-- 29 unsigned longwords -> 29 * 4 byte (4*8=32 bit) integers
     """
 
-    header_data["data_points"] = np.fromfile(fid, dtype=np.int32, count=29, offset=3196)
+    header_data["data_points"] = np.fromfile(fid, dtype=np.uint32, count=29, offset=3196)
     """
     %-- (3313-3428) Lower and upper boundary of FFT number in each combined channel:
     %-- 2 * 29 * 2 bytes (2*8=16 bits)
@@ -230,7 +230,7 @@ def _get_header_data(fid):
     %-- 29 unsigned longwords -> 29 * 4 byte (4*8=32 bit) integers
     """
 
-    header_data["rx_frequency_offset"] = np.fromfile(fid, dtype=np.int32, count=29, offset=3660)
+    header_data["rx_frequency_offset"] = np.fromfile(fid, dtype=np.uint32, count=29, offset=3660)
     """
     %-- (3777-3808) FIR coefficient in RX: 16 * 2 bytes (2*8=16 bits)
     %-- Doesn't fit into the head matrix: 16 numbers into 8 slots...
@@ -254,13 +254,13 @@ def _get_header_data(fid):
     %-- 29 unsigned longwords -> 29 * 4 byte (4*8=32 bit) integers
     """
 
-    header_data["cic_rx_filter_amount"] = np.fromfile(fid, dtype=np.int32, count=29, offset=3940)
+    header_data["cic_rx_filter_amount"] = np.fromfile(fid, dtype=np.uint32, count=29, offset=3940)
     """
     %-- (4057-4172) CIC cropping rate in RX for each combined channel:
     %-- 29 unsigned longwords -> 29 * 4 byte (4*8=32 bit) integers
     """
 
-    header_data["cic_rx_cropping_rate"] = np.fromfile(fid, dtype=np.int32, count=29, offset=4056)
+    header_data["cic_rx_cropping_rate"] = np.fromfile(fid, dtype=np.uint32, count=29, offset=4056)
     """
     %-- (4173-4176) Above sea level:
     """
@@ -275,7 +275,7 @@ def _get_header_data(fid):
     %-- (4181-4260) Comment by user: 80 bytes (80*8=640 bits)
     """
 
-    header_data["user_comment"] = decode_utf(np.fromfile(fid, dtype='S80', count=1, offset=4180))
+    header_data["user_comment"] = _decode_utf(np.fromfile(fid, dtype='S80', count=1, offset=4180))
     """
     %-- (4261-4272) The next 3 parameters are 4 bytes (4*8=32 bits) each
     """
@@ -285,7 +285,7 @@ def _get_header_data(fid):
     %-- (4273-4480) User header: 4480-4273+1 = 208 bytes
     """
 
-    header_data["user_header"] = decode_utf(np.fromfile(fid, dtype='S208', count=1, offset=4272))
+    header_data["user_header"] = _decode_utf(np.fromfile(fid, dtype='S208', count=1, offset=4272))
     return header_data
 
 
@@ -293,18 +293,18 @@ def _get_header_data(fid):
 # It first views it as a chararray to access the decode method,
 # then squeezes it and converts it from a ndarray into a python string.
 # After this, I remove the ' at the beginning and end of the string.
-def decode_utf(ucs_str):
+def _decode_utf(ucs_str):
     return np.array2string(ucs_str.view(np.chararray).decode('utf-8').squeeze())[1:-1]
 
 # Since the datetimes involved are given in the form "DD-MMM-YYYY hh:mm:ss.ss"
 # and numpy expects the form "YYYY-MM-DD hh:mm:ss.ss(...)", we must first convert
 # the MU date to numpy standard format. This is accomplished with the help of the
-# month dictionary and the decode_utf function.
+# month dictionary and the _decode_utf function.
 
 
-def convert_date(date_str):
+def _convert_date(date_str):
     if type(date_str) == np.ndarray:
-        date_str = decode_utf(date_str)
+        date_str = _decode_utf(date_str)
     return date_str[7:11] + "-" + month[date_str[3:6]] + "-" + date_str[0:2] + date_str[11:]
 
 
@@ -326,14 +326,14 @@ month = {
 # Checks so the date hasn't rolled over, and if it has it adds a day to the counter.
 
 
-def fix_date_edge_case(start_time, end_time):
+def _fix_date_edge_case(start_time, end_time):
     if end_time - start_time < 0:
         return end_time + np.timedelta64(1, 'D')
     return end_time
 
 def convert_MUI_to_h5(in_path, out_path):
 
-    #header_data = _get_header_data(stuff)
+    header_data = _get_header_data(in_path)
     #
     #file.attrs['file_end_time'] = 0
     pass
