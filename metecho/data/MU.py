7fa86cea92a3e1c63566cf1981e50c9c554041e8
kandarian <kenneth@kullbrandt.se>
2021-09-30 10:39:18 +0200

    moved plotting to it's own subpackage

--- metecho/data/MU.py
+++ metecho/data/MU.py
@@ -468,29 +468,3 @@ def convert_MUI_to_h5(filepath, experiment_name="mw26x6", output_location=None, 
     logger.debug(f'Reached EOF, exiting loop and closing file')
     file.close()
     return file_outputs_created
-
-
-def _plot_colormesh(filepath, output_filepath=""):
-    """
-    Simple function to check the data. Filepath needs a h5 file.
-    """
-    try:
-        h5file = h5py.File(filepath, 'r')
-    except FileNotFoundError:
-        logger.exception(f'Could not open file: {filepath}. File does not exist.')
-        return
-
-    dset = h5file["data"]
-    powsum = np.abs(np.sum(dset, axis=0))**2
-    """
-    Sets pyplot to classic rendering, then renders the powersum unto it. We then add
-    a colorbar and the y and x-label before saving it. At the moment it only saves
-    to plot.png.
-    """
-    plt.style.use('classic')
-    plt.pcolormesh(powsum)
-    plt.colorbar()
-    plt.ylabel('signal strength')
-    plt.xlabel('time')
-    plt.savefig(output_filepath + 'plot.png')
-    plt.close('all')
