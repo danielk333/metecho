09659bc3fd335d815c310ac53235c56c2312e00f
kandarian <kenneth@kullbrandt.se>
2021-10-06 09:40:56 +0200

    made sure input variables work

--- metecho/generalized_matched_filter/barker_xcorr_decode_single.c
+++ metecho/generalized_matched_filter/barker_xcorr_decode_single.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include "hdf5.h"
-#include "hdf5_hl.h"
-/* Macro for making Keyword Arguments possible */
-#define decode_single(h5_file_path, ...) decode_single_base(h5_file_path, (struct input_data){.channel_decode = false, __VA_ARGS__ })
+#include <complex.h>
+#define decode_single(ipp, raw_data, size, ...) decode_single_base(ipp, raw_data, size, (struct input_data){.channel_decode = false, __VA_ARGS__ })
+#define complex_size(...) sizeof(__VA_ARGS__)/2
 
 struct input_data {
     bool channel_decode;
 };
 
-void decode_single_base(char *h5_file_path, struct input_data data){
-    hid_t           file; /* File identifier */
-    herr_t          status;
-
-    /* Opens the h5 file in Read Only mode. */
-    file = H5Fcreate(h5_file_path, H5F_ACC_RDONLY, H5P_DEFAULT, H5P_DEFAULT);
-    /* Closes the file. */
-    status = H5Fclose(file);
-
-}
+void decode_single_base(int ipp, complex *raw_data, int *size, struct input_data data){
+    printf("%li\n", complex_size(raw_data));
+};
 
 int main() {
-    decode_single("/mnt/e/Kurser/X7007E/data/2009/06/27/2009-06-27T09.54.05.690000000.h5", .channel_decode=true);
+    complex testinput[] = {1.0 + 3.0 * I, 5.0 + 2.0 * I, 3.0 + 6.0 * I, 4.0 + 4.0 * I};
+    int sizeinput[] = {5, 5, 2, 3};
+    int ipp = 1337;
+    decode_single(ipp, testinput, sizeinput, .channel_decode=true);
     return 0;
-}
+};
