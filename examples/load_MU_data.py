496401ef2b15d35279b873c9e8a1fd81aa4c015f
danielk <daniel.kastinen@irf.se>
2021-10-06 08:30:39 +0200

    tried to reformat plotting to that of oop-mpl style

---
+++ examples/load_MU_data.py
@@ -0,0 +1,24 @@
+import pathlib
+import matplotlib.pyplot as plt
+
+import metecho
+
+base_path = pathlib.Path('/home/danielk/IRF/data/MU_TEST_DATA_SET/MUI/')
+
+h5_mu_file = base_path / '2009/06/27/2009-06-27T09.54.05.690000000.h5'
+
+if not h5_mu_file.is_file():
+    files = metecho.data.mu.convert_MUI_to_h5(
+        [base_path / 'MUI.090627.095404'], 
+        output_location = str(base_path),
+        skip_existing = False,
+    )
+
+    print(files)
+
+
+raw = metecho.data.RawDataInterface(h5_mu_file)
+
+metecho.plot.rti(raw, output_path=base_path / f'2009/06/27/{".".join(raw.path.name.split(".")[:-1]) + ".png"}')
+
+plt.show()
