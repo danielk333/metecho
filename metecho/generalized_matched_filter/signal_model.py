b5a7ace0b51277a0846934d00a6f1acccc9499b5
= <=>
2021-11-10 15:27:33 +0100

    made pip installable with setuptools extensions

--- metecho/generalized_matched_filter/signal_model.py
+++ metecho/generalized_matched_filter/signal_model.py
@@ -1,12 +1,11 @@
 import numpy as np
 
 
-encode = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, 1, 1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1],
-                  dtype=np.float64)
+barker13 = np.array([1, 1, 1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1],
+                    dtype=np.complex128)
 
 
-def getModel(code_name):
-    if code_name == "barker":
-        return encode
-
-    raise ValueError(f'No model found for {code_name}')
+def barker_code_13(pulse_amount, scale):
+    return_value = np.tile(np.kron(barker13, np.ones(scale)), pulse_amount)
+    return_value.shape = (pulse_amount, 13 * scale)
+    return return_value
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import setuptools
 import codecs
 import os.path
+from distutils.core import Extension
 
 
 with open('requirements', 'r') as fh:
@@ -10,12 +11,16 @@ with open('requirements', 'r') as fh:
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-#from
-#https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
+# from
+# https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
+
+
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
     with codecs.open(os.path.join(here, rel_path), 'r') as fp:
         return fp.read()
+
+
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
         if line.startswith('__version__'):
@@ -25,25 +30,31 @@ def get_version(rel_path):
         raise RuntimeError("Unable to find version string.")
 
 
+libecho = Extension(name='metecho.generalized_matched_filter.libecho',
+                    sources=['metecho/generalized_matched_filter/xcorr_echo_search.c']
+                    )
+
+
 setuptools.setup(
-    name = 'metecho',
-    version = get_version("metecho/version.py"),
-    long_description = long_description,
-    long_description_content_type = 'text/markdown',
-    url = 'https://github.com/danielk333/metecho',
-    classifiers = [
+    name='metecho',
+    version=get_version("metecho/version.py"),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/danielk333/metecho',
+    classifiers=[
         'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Intended Audience :: Science/Research',
         'Development Status :: 4 - Beta',
     ],
-    python_requires = '>=3.0',
-    install_requires = pip_req,
-    packages = setuptools.find_packages(),
+    python_requires='>=3.0',
+    install_requires=pip_req,
+    packages=setuptools.find_packages(),
+    ext_modules=[libecho],
     # metadata to display on PyPI
-    author = 'Daniel Kastinen',
-    author_email = 'daniel.kastinen@irf.se',
-    description = 'Keplerian orbit functions in Python',
-    license = 'MIT',
+    author='Daniel Kastinen',
+    author_email='daniel.kastinen@irf.se',
+    description='Keplerian orbit functions in Python',
+    license='MIT',
 )
