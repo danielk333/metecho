c36cf2dc86b389a0a411833d59a75f7c6921deac
danielk <daniel.kastinen@irf.se>
2022-01-26 13:26:47 +0100

    allow for automatic choise of converter target backend

--- src/metecho/data/converters.py
+++ src/metecho/data/converters.py
@@ -62,7 +62,7 @@ def converter_validator(name):
 
 
 @tools.profiling.timeing(f'{__name__}')
-def convert(input_files, output_location, backend, input_format=None, **kwargs):
+def convert(input_files, output_location, backend=None, input_format=None, **kwargs):
     '''Convert given list of input files or input file to a supported 
     backend format and returns the created data files.
     '''
@@ -80,16 +80,25 @@ def convert(input_files, output_location, backend, input_format=None, **kwargs):
             valid = [validator(file) for file in input_files]
             if np.any(valid):
                 sub_files = [input_files[x] for x in np.argwhere(valid).flatten()]
-                if backend not in backends:
+                if backend is None:
+                    use_backend = None
+                    for key in backends:
+                        if key == 'validator':
+                            continue
+                        use_backend = key
+
+                elif backend not in backends:
                     raise ValueError(
                         f'No converter to the given backend \
                         "{backend}" found for given input path type "{fmt}"\n \
                         Files affected: {sub_files}'
                     )
+                else:
+                    use_backend = backend
 
-                logger.info(f'Converting {len(sub_files)} from {fmt} to {backend}...')
+                logger.info(f'Converting {len(sub_files)} from {fmt} to {use_backend}...')
 
-                func = backends[backend]
+                func = backends[use_backend]
 
                 files_created = func(sub_files, output_location, **kwargs)
                 format_found = True
@@ -103,8 +112,18 @@ def convert(input_files, output_location, backend, input_format=None, **kwargs):
         if input_format not in CONVERTERS:
             raise ValueError(f'Given input format {input_format} has no converters')
 
-        logger.info(f'Converting {len(input_files)} from {input_format} to {backend}...')
-        func = CONVERTERS[input_format][backend]
+        backends = CONVERTERS[input_format]
+        if backend is None:
+            use_backend = None
+            for key in backends:
+                if key == 'validator':
+                    continue
+                use_backend = key
+        else:
+            use_backend = backend
+        
+        logger.info(f'Converting {len(input_files)} from {input_format} to {use_backend}...')
+        func = CONVERTERS[input_format][use_backend]
         files_created = func(input_files, output_location, **kwargs)
 
     return files_created
