e39828bb1126a8f4719d3fa291c53e3c7c8ff1b7
danielk <daniel.kastinen@irf.se>
2022-03-04 15:33:01 +0100

    feature of latest setuptools

--- setup.py
+++ setup.py
@@ -1,28 +1,5 @@
 import setuptools
-import codecs
-import pathlib
 from distutils.core import Extension
-
-HERE = pathlib.Path(__file__).resolve().parents[0]
-
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
-
-def get_version(path):
-    '''Read version from python version file
-    
-    Code originally from:
-    https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
-    '''
-    with codecs.open(path, 'r') as fp:
-        for line in fp.read().splitlines():
-            if line.startswith('__version__'):
-                delim = '"' if '"' in line else "'"
-                return line.split(delim)[1]
-        else:
-            raise RuntimeError("Unable to find version string.")
-
 
 libxcorr = Extension(
     name='metecho.generalized_matched_filter.libxcorr',
@@ -30,8 +7,6 @@ libxcorr = Extension(
     include_dirs=['src/libxcorr/'],
 )
 
-
 setuptools.setup(
-    version=get_version(HERE / 'src' / 'metecho' / 'version.py'),
     ext_modules=[libxcorr],
 )
