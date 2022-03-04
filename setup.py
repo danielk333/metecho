034a0c2539959e6503b0ceb7613700b3d169f9fa
danielk <daniel.kastinen@irf.se>
2022-03-04 18:08:15 +0100

    single sourcing still broken, lets take this later

--- setup.py
+++ setup.py
@@ -1,20 +1,5 @@
 import setuptools
 from distutils.core import Extension
-import pathlib
-import codecs
-
-HERE = pathlib.Path(__file__).resolve().parents[0]
-
-
-def get_version(path):
-    with codecs.open(path, 'r') as fp:
-        for line in fp.read().splitlines():
-            if line.startswith('__version__'):
-                delim = '"' if '"' in line else "'"
-                return line.split(delim)[1]
-        else:
-            raise RuntimeError("Unable to find version string.")
-
 
 libxcorr = Extension(
     name='metecho.generalized_matched_filter.libxcorr',
@@ -23,6 +8,5 @@ libxcorr = Extension(
 )
 
 setuptools.setup(
-    version=get_version(HERE / 'src' / 'metecho' / 'version.py'),
     ext_modules=[libxcorr],
 )
