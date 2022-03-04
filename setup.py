9e0d9f9b694b44dbabd450f456072c59bb5eb8c6
danielk <daniel.kastinen@irf.se>
2022-03-04 17:30:01 +0100

    maybe its a python version thing?

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import Extension
 
 libxcorr = Extension(
     name='metecho.generalized_matched_filter.libxcorr',
-    sources=['src/libxcorr/libxcorr.c', 'src/libxcorr/libxcorr.h'],
+    sources=['src/libxcorr/libxcorr.c'],
+    include_dirs=['src/libxcorr/'],
 )
 
 setuptools.setup(
