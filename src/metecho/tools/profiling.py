3d600633dea3d6c4e7388ab1f3a06f535abe5cf4
danielk <daniel.kastinen@irf.se>
2022-03-23 08:02:06 +0100

    pep8

--- src/metecho/tools/profiling.py
+++ src/metecho/tools/profiling.py
@@ -31,38 +31,34 @@ class BaseProfiler:
         self.add(name, dt)
         return dt
 
-
     def init(self, name, enabled):
         self.names.append(name)
         self.enabled[name] = enabled
         self.executions[name] = 0
         self.times[name] = 0.0
 
-
     def add(self, name, dt):
         if self.enabled[name]:
             self.executions[name] += 1
             self.times[name] += dt
 
-
     def __str__(self):
         means = self.means()
         max_len = max([len(x) for x in self.names])
         rows = [
-            f'{name:<{max_len}} @ {self.executions[name]} x {means.get(name,0):.3e} s = {self.times[name]:.3e} s total'
-                for name in self.names
-                if self.executions[name] > 0
+            f'{name:<{max_len}} @ {self.executions[name]} x \
+{means.get(name,0):.3e} s = {self.times[name]:.3e} s total'
+            for name in self.names
+            if self.executions[name] > 0
         ]
         return '\n'.join(rows)
 
-
     def means(self):
         return {
-            name:self.times[name]/self.executions[name]
-                for name in self.names 
-                if self.executions[name] > 0
-            }
-
+            name: self.times[name]/self.executions[name]
+            for name in self.names 
+            if self.executions[name] > 0
+        }
 
     def enable(self, prefix):
         for name in self.names:
@@ -85,13 +81,13 @@ def timeing(prefix, enabled=False):
         def timed_func(*args, **kwargs):
             if PROFILER.enabled[name]:
                 t0 = time.time()
-    
+
             ret = func(*args, **kwargs)
-    
+
             if PROFILER.enabled[name]:
                 dt = time.time() - t0
                 PROFILER.add(name, dt)
-            
+
             return ret
 
         return timed_func
