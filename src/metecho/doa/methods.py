0dba397b745316040e934c27a688c10f9b8307ff
thijs_abc <thijsalexander@outlook.com>
2022-04-04 09:48:44 +0200

    did some work on methods.py, but still not complete

--- src/metecho/doa/methods.py
+++ src/metecho/doa/methods.py
@@ -14,44 +14,40 @@ from math import floor
 
 h5_mu_file = pathlib.Path('.').home() / 'clones' / 'metecho_clone' / 'metecho' / 'docs' / 'source' / 'examples' / 'data' / 'MU_h5' / '2009' / '06' / '27' / '2009-06-27T09.54.05.690000000.h5'
 
-raw_data = metecho.data.RawDataInterface(h5_mu_file)
+raw = metecho.data.RawDataInterface(h5_mu_file)
+raw_data = raw.data
+
+print('raw_data type: ', type(raw.data))
+print('raw_data pulse count: ', (raw_data.shape[raw.axis["pulse"]]))
+print('raw_data: ', (raw_data))
+print('raw_data_channel: ', raw.axis['channel'])
+
+print('test1: ', np.shape(raw.data[0]))
+print('test2: ', np.shape(raw.data[raw.axis['channel']]))
+
 
 #start_values = matched_filter_output['best_start']
 start_values = np.load('indecies.npy')
 ipps = np.arange(len(start_values))
-
 ipp_range = [190, 260]
-start_values_existance = np.isnan(start_values, where=False)
 
-
-indecies = start_values_existance
-print('indecies: ', indecies)
-
-#indecies = matched_filter_output['best_start']
-
-# IPP_n gives np.array with all element numbers from existant start_values
 data_selector = np.logical_and.reduce([
     ipps >= ipp_range[0], 
     ipps <= ipp_range[1],
     np.logical_not(np.isnan(start_values)),
 ])
-print('data_selector: ', data_selector)
 
-DATA_n = start_values[data_selector]
+# IPP_n gives np.array with all element numbers from existant start_values
 IPP_n = ipps[data_selector]
 
-print('DATA_n: ', DATA_n)
-print('IPP_n: ', IPP_n)
-
-# for i, j in zip(indecies, start_values):
-#     counter += 0
-
-#     if i == True:
-#         IPP_n = np.append(IPP_n, count)
-#         DATA_n = np.append(DATA_n, j)
-
 # Data_n gives np.array with all existant start_values
+DATA_n = start_values[data_selector]
 DATA_n = np.floor(DATA_n)
+
+print('data_selector: ', data_selector)
+print('IPP_n: ', IPP_n)
+print('DATA_n: ', DATA_n)
+
 
 simultaneous_meteors = 1
 
@@ -63,8 +59,11 @@ elevation = np.empty(len(IPP_n)).fill(np.NaN)
 k_vector = np.empty(
     (3, len(IPP_n))
 ).fill(np.NaN)
-shape = (raw_data.radar.beam.channels, len(IPP_n))
-eigs = np.empty(shape).fill(np.NaN)
+
+# note to self: the fck should i solve this?? what does this look like in the matlab code??
+eigs = np.empty((raw.data[raw.axis['channel']], len(IPP_n))).fill(np.NaN)
+eigs = np.empty((0, len(IPP_n))).fill(np.NaN)
+
 
 
 
