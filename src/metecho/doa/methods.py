f7e525dd735eea93d6a2b213aa79668230cd0caf
thijs_abc <thijsalexander@outlook.com>
2022-04-12 12:05:30 +0200

    Added temporary files for data saves used to plot multiple different grids and succesfull plotting of 1 grid, while atempted but not yet functional plotting of the maximums of several grids. Also added peak_finder functions and its relying components in functions.py, must be moved to different locations (probably pyant) at later stage when fully working. many programming errors not yet resolved for peak_finder. fminsrchbnd.m file not yet located in or ported to python.

--- src/metecho/doa/methods.py
+++ src/metecho/doa/methods.py
@@ -4,163 +4,12 @@ import pathlib
 import numpy as np
 from math import floor
 import pyant
+from pyant.functions import get_antenna_positions, calc_antenna_gain, calc_R_matrix, MUSIC_grid_search
+from matplotlib import pyplot as plt
+import sys
 
-
-# use IPP's to calc R-matrix func
-def R_matrix(raw_data, I, DATA_n, IPP_n, p):
-    """ Calculates R-matrix
-
-    """
-
-    # get number of sample values (85 for used data)
-    nhigh = raw.data.shape[raw.axis['sample']]
-    
-    # create 1D array for select range gates
-    select_range_gates = np.arange(DATA_n[I], DATA_n[I] + len(raw.meta['code']) + 1)
-
-
-    # option 1
-    if select_range_gates[-1] > nhigh:
-
-        # hopefully: if element in select_range_gates > nhigh, delete it from array
-        np.delete(select_range_gates, select_range_gates > nhigh)
-
-
-    # # option 2
-    # # if element in select_range_gates > nhigh, delete it from array
-    # select_range_gates = np.delete(select_range_gates, np.where(
-    #     (select_range_gates[-1] > nhigh) & (select_range_gates > nhigh))
-    # )
-
-
-
-    # calc samples_subset
-    samples_subset = squeeze(raw.data[raw.axis['sample']][:, select_range_gates, IPP_n[I]])
-
-    # calc R_matrix
-    R_matrix = (samples_subset @ samples_subset.T) / np.shape((samples_subset, 1))
-    
-
-    return R_matrix
-
-
-def MUSIC_grid_search(R_matrix, num, radar, **options):
-    """ creates a grid for 1 ore more multiple simultanious meteors.
-
-    INPUT: 
-
-    # covariance matrix
-    R_matrix = np.array([])
-    # resolution
-    num = int
-    radar = 
-
-    """
-    
-#     options_array = np.array([
-#         ['met_n', 'restriction', 'elevation'], 
-#         [1, np.array([[-1, 1],[-1, 1]]), 0]])
-
-# # this doenst work of course, but what would?
-#     # for option, std_value in options_array:
-#     #     if option in options:
-#     #        options[option] = value
-
-
-
-    # number of meteors to analyse simultaniously
-    if 'met_n' not in options:
-        met_n = 1
-
-    # area of signal range in which to analyse results
-    if 'restriction' not in options:    
-    restriction = np.array([[-1, 1],[-1, 1]])
-
-    # angle of elevation
-    if 'elevation' not in options:
-    elevation = 0
-
-
-    N = np.shape(R_matrix, 1)
-
-    # Compute eigendecomposition of covariance matrix
-    Q ,D = np.linalg.eig(R_matrix)
-
-    # Find r largest eigenvalues
-    I = numpy.sort(np.diag(D))[::-1]
-
-    # # n maximum indecies
-    # n = 2
-    # indecies = (-D).argsort()[:n]
-    # I = Q[indecies]
-
-
-    kx, ky = np.meshgrid(np.linspace(restriction(1, 1),restriction(1, 2), num), 
-        np.linspace(restriction(2, 1),restriction(2, 2), num))
-
-    kz = np.sqrt(1 - kx**2 - ky**2)
-
-    # Sort the eigenvectors to put signal eigenvectors first
-    Q = Q[:, I]
-
-    # Get the signal eigenvectors
-    # Qs = Q (:,1:met_n)
-
-    if met_n > 1:
-
-        Qn = np.empty((met_n, 1))
-        Qn.fill(np.NaN)
-
-        for ind in np.arange(1, met_n + 1):
-            Qn[ind] = Q[:, 
-            np.array(
-                [np.array(1, [ind-1] + 1), np.arange([ind+1], N + 1)]
-                )
-            ]
-    else:
-
-        # Get the noise eigenvectors
-        Qn = Q[:, np.arange(2, N + 1)]
-
-    if met_n > 1:
-
-        F_vals = np.zeros([num, num, met_n])
-        
-        for xi in np.arange(1, num + 1):
-
-            for yi in np.arange(1, num + 1):
-
-                if kx[xi, yi]**2 + ky[xi, yi]**2 <= np.cos(elevation_limit):
-
-                    a = pyant.Array.complex(k=
-                        [kx[xi, yi], 
-                        ky[xi, yi], 
-                        kz[xi, yi]].T
-                        )
-                    
-                    for ind in np.arange(1, met_n + 1):
-
-                        F_vals[xi, yi, ind] = (a.T @ a) / (a.T @ Qn[ind] @ Qn[ind].T @ a)
-        
-
-    else:
-        F_vals = np.zeros([num, num])
-        
-        for xi in np.arange(1, num + 1):
-
-            for yi in np.arange(1, num + 1):
-
-                if kx[xi, yi]**2 + ky[xi, yi]**2 <= np.cos(elevation_limit):
-
-                    a = pyant.Array.complex(k=
-                        [kx(xi, yi), 
-                        ky(xi, yi), 
-                        kz(xi, yi)].T
-                        )
-
-                    F_vals[xi, yi] = (a.T @ a) / (a.T @ Qn @ Qn.T @ a)
-
-    return F_vals, kx, ky
+# numpy option to print full numpy array instead of truncation
+np.set_printoptions(threshold=sys.maxsize)
 
 
 # MUSIC
@@ -175,25 +24,17 @@ h5_mu_file = pathlib.Path('.').home() / 'clones' / 'metecho_clone' / 'metecho' /
 raw = metecho.data.RawDataInterface(h5_mu_file)
 raw_data = raw.data
 
-print('raw_data shape: ', np.shape(raw.data))
-print('raw_data pulse count: ', (raw_data.shape[raw.axis["pulse"]]))
-print('raw_data: ', (raw_data))
-print('raw_data_channel: ', raw.axis['channel'])
+
+# instantiate the beam
+radarsystem = 'MU_radar'
+antenna_pos = get_antenna_positions(radarsystem, requested_positions='req_ant_pos')
+beam = calc_antenna_gain(radarsystem, antenna_positions=antenna_pos[0])[0]
+
 
 #indexing = [slice(), slice(), slice()]
 #indexing[raw.axis['channel']] = 1
 
-print('test1: ', np.shape(raw.data[0,7,9]))
-print('test2: ', np.shape(raw.data[raw.axis['channel']]))
-#print('test2: ', np.shape(raw.data[tuple(indexing)]))
-print('test3: ', raw.data[raw.axis['sample']])
-#print('test4: ', raw.data.radar)
-print('nhigh: ', raw.data.shape[raw.axis['sample']])
-print('test 5: ', np.arange(0,10,1)[-2])
-print('radar.encoding.bits: ', len(raw.meta['code']))
 
-print('samples_len: ', len(raw.data[raw.axis['sample']]))
-print('samples: ', raw.data[raw.axis['sample']])
 
 #start_values = matched_filter_output['best_start']
 start_values = np.load('indecies.npy')
@@ -203,7 +44,8 @@ ipp_range = [190, 260]
 data_selector = np.logical_and.reduce([
     ipps >= ipp_range[0], 
     ipps <= ipp_range[1],
-    np.logical_not(np.isnan(start_values)),
+    np.logical_not(np.isnan(start_values)
+        ),
 ])
 
 # IPP_n gives np.array with all element numbers from existant start_values
@@ -213,12 +55,8 @@ IPP_n = ipps[data_selector]
 DATA_n = start_values[data_selector]
 DATA_n = np.floor(DATA_n)
 
-print('data_selector: ', data_selector)
-print('IPP_n: ', IPP_n)
-print('DATA_n: ', DATA_n)
-
-
 simultaneous_meteors = 1
+
 
 # create storage arrays for numerous parameters/results
 MUSIC_peaks = np.empty(len(IPP_n))
@@ -238,37 +76,114 @@ eigs = np.empty(
     )
 eigs.fill(np.NaN)
 
-
-
-
 k_vector_out = np.empty((3, 1))
 k_vector_out.fill(np.NaN)
 
 
+# function calls for testing if making 1 grid works
+
+R_matrix = calc_R_matrix(raw, 10, DATA_n, IPP_n) 
+
+print('R_matrix_shape: ', R_matrix.shape)
+print('r_matrix_type: ', type(R_matrix))
+
 F_vals_all, kx, ky = MUSIC_grid_search(
-    R_matrix, 
-    resolution, 
-    raw_data.radar
+    R_matrix,
+    beam,  
+    num=200, 
     )
 
+print('F_vals_all_shape: ', np.shape(F_vals_all))
+print('kx_shape: ', np.shape(kx))
+print('ky_shape: ', np.shape(ky))
+
+# # save data in temp npy file
+# np.save('temp_grid_data', arr=[F_vals_all, kx, ky])
 
 
 
 
 
-for I in range(0, len(IPP_n) - 1):
+max_f_val_array = np.array([[],[]])
+x_max_array = np.array([])
+y_max_array = np.array([])
 
-    # calc k_vector values
-    k_vector_out = None
 
-    #eigs = np.append(eigs, i)
-    # eigs(:,i) = eigs_out;
-    # MUSIC_peaks(i,:) = transpose(peaks_out(:))
-    # azimuth(i,:) = transpose(azimuth_out(:))
-    # elevation(i,:) = transpose(elevation_out(:))
+for I in range(0, 2 - 1):
+#for I in range(0, len(IPP_n) - 1):
+
+    F_vals_all, kx, ky = MUSIC_grid_search(
+    R_matrix,
+    beam,  
+    num=200, 
+    )
+
+    print('F_vals_all: ', F_vals_all)
+
+    x, y = kx[0, :], ky[:, 0]
+
+    # calc maximum of F_vals_all to find meteor loc
+    max_f_val = np.amax(F_vals_all)
+
+    # calc max value indices
+    max_index = np.where(F_vals_all == max_f_val)
+    print('max_index: ', max_index)
+
+    # get x index max and y index max
+    x_max_index = np.asscalar(max_index[0])
+    y_max_index = np.asscalar(max_index[1])
+
+    # actual x and y values for max
+    x_max = x[np.asscalar(max_index[0])]
+    y_max = y[np.asscalar(max_index[1])]
+
+
+    max_f_val_array = np.append(max_f_val_array, F_vals_all[x_max_index, 0], axis=0)
+    max_f_val_array = np.append(max_f_val_array, F_vals_all[0, y_max_index], axis=1)
+    x_max_array = np.append(x_max_array, x_max)
+    y_max_array = np.append(y_max_array, y_max)
+
+
+# save data in temp npy file
+np.save('temp_multi_grid_data', arr=[max_f_val_array, x_max_array, y_max_array])
+
+
+
+
+
+# # down here when peak finder is implemented, a number of grids is generated in 
+# # the process of calculating k_vector_out
+
+# for I in range(0, len(IPP_n) - 1):
+
+#     F_vals_all, kx, ky = MUSIC_grid_search(
+#     R_matrix,
+#     beam,  
+#     num=200, 
+#     )
+
+#     x, y = kx[0, :], ky[:, 0]
+
+#     # calc maximum of F_vals_all to find meteor loc
+#     max_f_val = np.maximum(F_vals_all)
+
+#     print('max_f_val: ', max_f_val)
+
+#     x_max, y_max = np.argmax(F_vals_all), 
+
+#     # calc k_vector values
+#     k_vector_out = None
+
+#     #eigs = np.append(eigs, i)
+#     # eigs(:,i) = eigs_out;
+#     # MUSIC_peaks(i,:) = transpose(peaks_out(:))
+#     # azimuth(i,:) = transpose(azimuth_out(:))
+#     # elevation(i,:) = transpose(elevation_out(:))
     
-    for dim in range(0,2):
-        k_vector[dim, I] = k_vector_out
+#     for dim in range(0,2):
+#         k_vector[dim, I] = k_vector_out
+
+
 
 
 
