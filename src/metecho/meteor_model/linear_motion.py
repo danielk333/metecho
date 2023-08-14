1e69775d320dcc9d1bc377445f38692da0229d69
danielk <daniel.kastinen@irf.se>
2023-08-14 09:51:18 +0200

    renaming meteor model and adding ablation modelling options

--- src/metecho/meteor_model/linear_motion.py
+++ src/metecho/meteor_model/linear_motion.py
@@ -6,8 +6,8 @@ from .model import Model
 
 
 class LinearMotion(Model):
-    '''Base linear motion model to be subclassed with a velocity function
-    '''
+    """Base linear motion model to be subclassed with a velocity function"""
+
     def __init__(self, p0, r0, vel_params, **kwargs):
         super().__init__(**kwargs)
         self.p0 = p0
@@ -17,8 +17,8 @@ class LinearMotion(Model):
     def evaluate(self, t, **kwargs):
         dist = self.integral_velocity(t)
         vel = self.velocity(t)
-        r = self.p0[:, None] + dist[None, :]*self.r0[:, None]
-        v = self.r0[:, None]*vel[None, :]
+        r = self.p0[:, None] + dist[None, :] * self.r0[:, None]
+        v = self.r0[:, None] * vel[None, :]
         return r, v
 
     @abstractmethod
@@ -31,11 +31,12 @@ class LinearMotion(Model):
 
 
 class ExpVelocity(LinearMotion):
-    '''Exponential velocity along a linear trajectory
-    '''
+    """Exponential velocity along a linear trajectory"""
 
     def velocity(self, t):
-        return self.vel_params[0] - self.vel_params[1]*np.exp(self.vel_params[2]*t)
+        return self.vel_params[0] - self.vel_params[1] * np.exp(self.vel_params[2] * t)
 
     def integral_velocity(self, t):
-        return self.vel_params[0]*t - (self.vel_params[1]/self.vel_params[2])*(np.exp(self.vel_params[2]*t) - 1)
+        return self.vel_params[0] * t - (self.vel_params[1] / self.vel_params[2]) * (
+            np.exp(self.vel_params[2] * t) - 1
+        )
